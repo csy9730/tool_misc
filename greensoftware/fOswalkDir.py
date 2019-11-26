c150f6a6eadb4b4130db1e504e3da5503dc56033
unknown <gd_csy@163.com>
2019-11-26 18:43:18 +0800

    update develop.md

--- greensoftware/fOswalkDir.py
+++ greensoftware/fOswalkDir.py
@@ -1,5 +1,7 @@
 import logging
-import time, os
+import time, os,sys
+import json
+import argparse
 
 def showFileProperties2(pth):
 	pth2 = pth.replace('/','_').replace('\\','_').replace(':','_')
@@ -20,11 +22,35 @@ def showFileProperties2(pth):
 				info += t
 				print (info)
 				fp.write(info+'\n')
+
+def showFileProperties2(path,output=None,depth=0,**kwargs):
+	output = output or "a.jsonline"
+	path = path or  "."
+	with open(output,'w',encoding="utf-8") as fp:
+		for root,dirs,files in os.walk(path):
+			print( root,dirs,files)
+			continue 
+			for filename in files:
+				state = os.stat(os.path.join(root,filename))
+				dct = { "path":os.path.join(root,filename),"isfolder":False ,
+					"size": state.st_size,
+					"atime": time.strftime("%Y-%m-%d %X",time.localtime(state[-3]))	,			
+					"mtime":time.strftime("%Y-%m-%d %X",time.localtime(state[-2])),				
+					"ctime": time.strftime("%Y-%m-%d %X",time.localtime(state[-1]))
+				 }					
+				fp.write(json.dumps(dct,ensure_ascii=False) +'\n')
+			for d in dirs:
+				dct = { "path":os.path.join(root,d),"isfolder":True,
+					"atime": time.strftime("%Y-%m-%d %X",time.localtime(state[-3]))	,			
+					"mtime":time.strftime("%Y-%m-%d %X",time.localtime(state[-2]))	,			
+					"ctime": time.strftime("%Y-%m-%d %X",time.localtime(state[-1]))
+				 }				
+				fp.write(json.dumps(dct,ensure_ascii=False) +'\n')
+
 if __name__== "__main__":
-    import sys
-    if len(sys.argv)>1:
-        pth = sys.argv[1]
-    else:
-        pth = r".."
-    print(pth)
-    showFileProperties2(pth)
+	ap = argparse.ArgumentParser(prog="oswalk")
+	ap.add_argument( dest = "path",help='a path ')
+	ap.add_argument("--output","-o",help="output filenames")
+	ap.add_argument("--depth","-d",type = int,help="depth level")
+	args = ap.parse_args()
+	showFileProperties2(**vars(args))
