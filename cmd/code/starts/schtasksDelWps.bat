0f4ad18514281a9eb608fcc7c00f5886d69f7689
chensy_zlg <gd_csy@163.com>
2020-02-10 18:15:04 +0800

    tiny update

--- cmd/code/starts/schtasksDelWps.bat
+++ cmd/code/starts/schtasksDelWps.bat
@@ -1,2 +1,8 @@
+taskkill /IM   wps.exe /IM wpscenter.exe /F
+echo "wps已经终止"&pause
 schtasks /delete  /f /tn WpsUpdateTask_admin
 schtasks /delete /f /tn WpsExternal_admin_20190428132612
+
+rem 查看所有任务
+schtasks |grep -i WpsExternal |cut -d ' ' -f1
+
